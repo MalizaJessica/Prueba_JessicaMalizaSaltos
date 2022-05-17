ecd616b0a0c6f1bb88ea98b9503b3496c88570e5
MalizaJessica <jessicamaliza19@gmail.com>
2022-05-16 20:10:33 -0500

    commit 20:10

--- app/src/main/java/fisei/uta/prueba_jessicamaliza/MainActivity.java
+++ app/src/main/java/fisei/uta/prueba_jessicamaliza/MainActivity.java
@@ -3,9 +3,18 @@ package fisei.uta.prueba_jessicamaliza;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText etNombres;
+    EditText etApellidos;
+    EditText etBase;
+    EditText etExponente;
+    EditText etFactorial;
+    EditText etPotencia;
+    Button btnSiguiente;
+    Button btnMostrarResultados;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
